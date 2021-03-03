d7e4562907ed6b2affb3193d6cf20fd256e71c59
penggs <pgs1108pgs@126.com>
2021-03-03 17:16:25 +0800

    spring-context

---
+++ spring-debug/src/main/java/org/example/debug/service/MessageService.java
@@ -0,0 +1,13 @@
+package org.example.debug.service;
+
+/**
+ * <project> spring
+ *
+ * <p>
+ *
+ * @author penggs
+ * @since 2021-03-02
+ */
+public interface MessageService {
+	String getMessage();
+}
---
+++ spring-debug/src/main/java/org/example/debug/service/impl/MessageServiceImpl.java
@@ -0,0 +1,17 @@
+package org.example.debug.service.impl;
+
+import org.example.debug.service.MessageService;
+
+/**
+ * <project> spring
+ *
+ * <p>
+ *
+ * @author penggs
+ * @since 2021-03-02
+ */
+public class MessageServiceImpl implements MessageService {
+	public String getMessage() {
+		return "hello world";
+	}
+}
